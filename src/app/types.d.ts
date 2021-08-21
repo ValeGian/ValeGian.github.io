471b4d53dc68df25ea8dad1011acdc4038467053
ValeGian <46gianninivalerio@gmail.com>
2021-08-21 13:29:35 +0200

    Big offline commit v2

---
+++ src/app/types.d.ts
@@ -0,0 +1,15 @@
+type Profile = {
+  platform: string,
+  link: string,
+  icon: string,
+  description: string
+}
+
+type Project = {
+  title: string,
+  description: string,
+  type: string,
+  date: any,
+  link: string
+  languages: string[]
+}
